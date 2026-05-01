44cb897a90a9f0f8f199891f8f2f00a9d8869965
Hackathon Dev <hackathon@example.com>
2026-05-01 21:36:26 +0530

    Fix jest haste module collision warning

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
+  modulePathIgnorePatterns: ['<rootDir>/.next/'],
 };
 
 export default createJestConfig(config);
